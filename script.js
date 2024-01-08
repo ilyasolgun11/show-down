05dd387e332c0fe3805d95d7fbb3460caca695bb
ilyasolgun11 <127804888+ilyasolgun11@users.noreply.github.com>
2024-01-08 22:32:00 +0000

    Add functionality so that the game logic function works

--- script.js
+++ script.js
@@ -50,20 +50,82 @@ const searchData = [
   },
 ];
 
+let firstHalfSearchVolumeValue;
+let secondHalfSearchVolumeValue;
+
 document.addEventListener("DOMContentLoaded", function () {
   startGame();
 });
 
-function startGame() {}
+function generateRandomNumber() {
+  return Math.floor(Math.random() * searchData.length);
+}
+
+// Declare 2 random numbers
+let randomNumber1;
+let randomNumber2;
+
+// Grab first half DOM elements
+let firstHalfSearchTerm = document.getElementById("first-half-search-term");
+let firstHalfSearchVolume = document.getElementById("first-half-search-volume");
+// Grab second half DOM elements
+let secondHalfSearchTerm = document.getElementById("second-half-search-term");
+let secondHalfSearchVolume = document.getElementById(
+  "second-half-search-volume"
+);
+
+function startGame() {
+  // Generate 2 random numbers
+  randomNumber1 = generateRandomNumber();
+  randomNumber2 = generateRandomNumber();
+  // Ensure the random numbers are not the same
+  while (randomNumber1 === randomNumber2) {
+    randomNumber2 = generateRandomNumber();
+  }
+  // Assign values to the DOM elements using random number
+  firstHalfSearchTerm.textContent = searchData[randomNumber1].searchTerm;
+  // Setting global variable value to searchData[randomNumber1].searchVolume
+  firstHalfSearchVolumeValue = searchData[randomNumber1].searchVolume;
+  firstHalfSearchVolume.textContent = firstHalfSearchVolumeValue;
+  secondHalfSearchTerm.textContent = searchData[randomNumber2].searchTerm;
+  // Setting global variable value to searchData[randomNumber2].searchVolume
+  secondHalfSearchVolumeValue = searchData[randomNumber2].searchVolume;
+  secondHalfSearchVolume.textContent = secondHalfSearchVolumeValue;
+  console.log(`First value is ${searchData[randomNumber1].searchVolume}`);
+  console.log(`Second value is ${searchData[randomNumber2].searchVolume}`);
+  console.log("Game started");
+}
 
 // Loop over buttons and see which one is clicked, based on higher or lower fire off choseHigher or choseLower functions
 const buttons = document.querySelectorAll(".btn");
 buttons.forEach((button) => {
   button.addEventListener("click", function () {
     if (button.getAttribute("data-type") === "higher") {
+      if (firstHalfSearchVolumeValue < secondHalfSearchVolumeValue) {
+        guessedCorrect();
+        console.log("Correct!");
+      } else {
+        console.log("Wrong");
+      }
       console.log("You clicked Higher");
     } else if (button.getAttribute("data-type") === "lower") {
       console.log("You clicked Lower");
     }
   });
 });
+
+function guessedCorrect() {
+  firstHalfSearchTerm.textContent = searchData[randomNumber2].searchTerm;
+  firstHalfSearchVolumeValue = searchData[randomNumber2].searchVolume;
+  firstHalfSearchVolume.textContent = firstHalfSearchVolumeValue;
+  console.log(
+    `first half search volume is now ${searchData[randomNumber2].searchVolume}`
+  );
+  randomNumber2 = generateRandomNumber();
+  secondHalfSearchTerm.textContent = searchData[randomNumber2].searchTerm;
+  secondHalfSearchVolumeValue = searchData[randomNumber2].searchVolume;
+  secondHalfSearchVolume.textContent = secondHalfSearchVolumeValue;
+  console.log(
+    `The second half search volume is now ${searchData[randomNumber2].searchVolume}`
+  );
+}
